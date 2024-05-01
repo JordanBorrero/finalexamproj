9d5c5358ec485aceeb6436639ebc2e3f512a3806
jordanborrero <jmb9380@psu.edu>
2024-05-01 16:32:38 -0400

    First Commit

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        String[] options = {"Game 1", "Game 2", "Game 3"}; //String array of the three game options
+        String[] options = {"Shape Matcher Game", "Quiz Controller Game", "Shape Shooter Game"}; //String array of the three game options
 
         //Creates dialog box that displays the game titles and descriptions and provides buttons that allow the user to select a game
         int response = JOptionPane.showOptionDialog(null,
-                "Select Game\nGame 1: Shape Matcher Game\nGame 2: Quiz Controller Game\nGame 3:Shape Shooter",
-                "Game Selection",
+                "Select Game:\n \nShape Matcher Game: Pick the correct name of the shape that is displayed on the screen before the timer runs out, in a series of levels that get more challenging\nQuiz Controller Game: Complete the math equations displayed on the screen for a chance to win a funny prize \nShape Shooter: Shoot the correct shape that corresponds to the shape that the player is assigned",
+                "Game Selection Menu",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
                 null, options, options[0]);
         switch (response) //switch statement that runs the game selected by the user
